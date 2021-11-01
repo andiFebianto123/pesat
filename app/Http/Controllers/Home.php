f05c391db876a73eb1ebec9f8f80558cee4dda55
murbianto <bian@rectmedia.id>
2021-11-01 12:16:40 +0700

    fitur filter data anak (front end)

--- app/Http/Controllers/Home.php
+++ app/Http/Controllers/Home.php
@@ -10,8 +10,39 @@ class Home extends Controller
 {
     public function index(Request $request){
         
-        $data['childs']     = ChildMaster::where('child_master.deleted_at',null)
-                              ->paginate(9);
+        $provinceid = $request->input('provinceid');
+        $class = $request->input('class');
+        $gender = $request->input('gender');
+        $childsdatas = ChildMaster::where('deleted_at',null);
+      
+        if($provinceid==null && $class==null &&  $gender == null ){
+
+        $child = $childsdatas->paginate(9);
+        }else{
+
+        if (isset($provinceid)) {
+           
+            $child = $childsdatas->where('province_id',$provinceid)
+                                ->paginate(9);
+        }
+        if($gender==1){
+            $child = $childsdatas->where('gender','LIKE',"%laki%")
+                                 ->paginate(9);
+            
+        }
+        if($gender==2){
+            $child = $childsdatas->where('gender','LIKE',"%perem%")
+                                 ->paginate(9);
+        }
+        if(isset($class)){
+            $child= $childsdatas->where('class',$class)
+                                ->paginate(9);
+        }
+
+    }
+        
+        $data['childs'] = $child;
+
         $data['provinces']  = Province::where('province.deleted_at',null)
                               ->get();
         $data['class']      = ChildMaster::where('child_master.deleted_at',null)
@@ -20,6 +51,13 @@ class Home extends Controller
 
         return view('Home',$data);
     }
+    // public function childdetail($id){
+
+    //     //$id = $request->input('childid');
+    //     dd($id);
+    //     return view('childdetail');
+
+    // }
 
 
 }
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Boostrap 5</title>
+    <title>PESAT</title>
     <link rel="stylesheet" href={{ asset('assets/css/bootstrap.min.css') }}>
     <link rel="stylesheet" href={{ asset('assets/font-awesome/css/font-awesome.css') }}>
     
@@ -54,7 +54,7 @@
             </div>
             <div class='col-4'>
                 <p>Gender :<p>
-                    <select class="form-select" aria-label="Default select example">
+                    <select class="form-select" name="gender" aria-label="Default select example">
                         <option selected></option>
                         <option value="1">Laki-Laki</option>
                         <option value="2">Perempuan</option>
@@ -62,7 +62,7 @@
             </div>
             <div class='col-4'>
                 <p>Kelas :<p>
-                    <select class="form-select" aria-label="Default select example">
+                    <select class="form-select" name = "class" aria-label="Default select example">
                         <option selected></option>
                         @foreach($class as $key => $data) 
                         <option value="{{$key}}">{{$key}}</option>
@@ -83,13 +83,21 @@
        </br>
     <div class="row">
     @foreach ($childs as $key => $child)
+    
     <div class="col-4">
-        <div class="card" style="width: 26rem; margin-bottom:30px;">
+    <a href="#" style="text-decoration:none;color: inherit;">
+    <input type="hidden" name="childid" value="{{$child->child_id}}">
+        <div class="card" style="width: 23rem;margin-left:25px; margin-bottom:30px;">
+        @if($child->photo_profile == '')
+        <img class="card-img-top" src="{{asset('storage/image/blank.png')}}" alt="Card image cap">
+        @else
             <img class="card-img-top" src="{{asset('storage/'.$child->photo_profile)}}" alt="Card image cap">
+        @endif
             <div class="card-body">
                 <h5 class="card-title">{{$child->full_name}}</h5>
             </div>
         </div>
+       </a>
        </div>
        
     @endforeach
@@ -112,7 +120,6 @@
        Silakan mengirimkan e-mail, telepon, atau join facebook kami:
           </div>
     <section class="mb-4">
-
       <ul class="nav justify-content-center">
         <li class="nav-item">
             <a class="nav-link link-light" aria-current="page" href="#"><i class="fa fa-envelope"></i> pesatjkt@gmail.com</a>
@@ -127,11 +134,9 @@
             <a class="nav-link link-light" href="#" tabindex="-1" aria-disabled="true"><i class="fa fa-phone"></i> 0821 1462 2245</a>
         </li>
       </ul>
-    
     </section>
-
             Copyright © 2017 PESAT. All rights reserved. | Syarat & Ketentuan
-       </footer>
+</footer>
       
 </body>
 </html>
