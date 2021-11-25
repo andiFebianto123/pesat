442d0720febb136a36c332bd92afad125cf166f0
murbianto <bian@rectmedia.id>
2021-11-25 15:04:45 +0700

    back end : perbaikan halaman dashboard

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -4,71 +4,53 @@
 <h1>Dashboard</h1>
 
 </br>
-    <div class="card" style="width:35%">
-		<div class="card-header">
-        Dashboard Summary        
+<div class="row">
+    <div class="col-2">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header" align="center">Jumlah Anak Yang Disponsori</div>
+        <div class="card-body">
+            <h5 class="card-title" align="center">{{$sponsored}}</h5>
+    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
         </div>
-		<div class="card-body">
-            <table>
-                <tr>
-                    <td>
-                        Jumlah Anak yang sudah disponsori
-                    </td>
-                    <td>
-                        :
-                    </td>
-                    <td>
-                   {{$sponsored}}
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Anak yang belum disponsori
-                    </td>
-                    <td>
-                        :
-                    </td>
-                    <td>
-                   {{$notsponsored}}
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Jumlah Uang
-                    </td>
-                    <td>
-                        :
-                    </td>
-                    <td>
-                     Rp. {{number_format($totalamount, 2, ',', '.')}}   
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        Jumlah Sponsor Baru Bulan ini
-                    </td>
-                    <td>
-                        :
-                    </td>
-                    <td>
-                        {{$newsponsor}}
-                    </td>
-                </tr>
-
-                <tr>
-                    <td>
-                        Sponsor Yang Belum Bayar
-                    </td>
-                    <td>
-                        :
-                    </td>
-                    <td>
-                        {{$notpaid}}
-                    </td>
-                </tr>
-
-            </table>
         </div>
-	</div>
+    </div>
+    <div class="col-2">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header" align="center">Jumlah Anak Yang Belum Disponsori</div>
+        <div class="card-body">
+        <h5 class="card-title" align="center">{{$notsponsored}}</h5>
+    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+        </div>
+    </div>
+    <div class="col-2">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header" align="center">Jumlah Uang</div>
+        <div class="card-body">
+        <h5 class="card-title" align="center">Rp. {{number_format($totalamount, 2, ',', '.')}}</h5>
+    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+        </div>
+    </div>
+
+    <div class="col-2">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header" align="center">Jumlah Sponsor Baru Bulan Ini</div>
+        <div class="card-body">
+        <h5 class="card-title" align="center">{{$newsponsor}}</h5>
+    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+        </div>
+    </div>
+
+    <div class="col-2">
+        <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
+        <div class="card-header" align="center">Sponsor Yang Belum Bayar</div>
+        <div class="card-body">
+        <h5 class="card-title" align="center">{{$notpaid}}</h5>
+    <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
+        </div>
+        </div>
+    </div>
+</div>
 @endsection
-
