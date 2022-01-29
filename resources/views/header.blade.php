e11e35d1063ee94abe70ad802db8dce2296514cb
andiFebianto123 <andifebianto123@gmail.com>
2022-01-29 09:22:36 +0700

    Menu burger fix

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -37,10 +37,24 @@
 <div class="navigation position-relative">
 <nav class="navbar navbar-expand-lg navbar-light bg-white">
   <div class="container-fluid">
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" style="order: 1;" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <div id="button-flex" style="order: 3;">
+      @if(auth()->user() == null)
+      <a href="{{route('sponsor.login')}}" style="text-decoration:none;color: inherit;">
+          <button class="btn btn-outline-primary" type="submit">Login</button>
+      </a>
+      <a href="{{url('register')}}" id="register">
+          <button class="btn btn-primary" type="submit">Register</button>
+      </a>
+      @else
+      <a href="{{url('my-account')}}" style="text-decoration:none;color: inherit;">
+          <button class="btn btn-primary" type="submit">Akun Saya</button>
+      </a>
+      @endif
+    </div>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03" style="order: 2;">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link" aria-current="page" href="{{url('sponsor/home')}}">Home</a>
@@ -55,18 +69,14 @@
             <a class="nav-link" aria-current="page" href="{{url('donate-goods')}}">Sponsor Barang</a>
         </li>
       </ul>
-      @if( auth()->user() == null )
-          <a href="{{route('sponsor.login')}}" style="text-decoration:none;color: inherit;">
-              <button class="btn btn-outline-primary" type="submit">Login</button>
-          </a>&nbsp
-          <a href="{{url('register')}}">
+        @if( auth()->user() == null )
+          <!-- <a href="{{url('register')}}">
               <button class="btn btn-primary" type="submit">Register</button>
-          </a>
-          @else
-          <a href="{{url('my-account')}}" style="text-decoration:none;color: inherit;">
-              <button class="btn btn-primary" type="submit">Akun Saya</button>
-          </a>&nbsp
-          @endif
+          </a> -->
+          <a href="{{url('register')}}" id="register2">
+            <button class="btn btn-primary" type="submit">Register</button>
+          </a>          
+        @endif
     </div>
   </div>
 </nav>
