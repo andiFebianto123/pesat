764ec2469fe1c9edb6b4f9c0f175280da04d0bb1
danang <danangekoyudanto1995@gmail.com>
2022-02-10 17:31:22 +0700

    dashboard responsive card

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -11,49 +11,86 @@
         </div>
     </div>
     <div class="row">
-        <div class="col-md-6 col-lg-2">
-            <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                <div class="card-header" align="center">Jumlah Anak Yang Disponsori</div>
+        <div class="col-md-6 col-lg-3 mb-4 flex">
+            <div class="card text-dark bg-light mb-0 h-100">
+                <div class="card-header" align="center">
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 60px;">
+                        Jumlah Anak Yang Disponsori
+                    </div>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title" align="center">{{ $sponsored }}</h5>
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 100px;">
+                        <h5 class="card-title" align="center">{{ $sponsored }}</h5>
+                    </div>
                     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-lg-2">
-            <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                <div class="card-header" align="center">Jumlah Anak Yang Belum Disponsori</div>
+        <div class="col-md-6 col-lg-3 mb-4 flex">
+            <div class="card text-dark bg-light mb-0 h-100">
+                <div class="card-header" align="center">
+
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 60px;">
+                        Jumlah Anak Yang Belum Disponsori
+                    </div>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title" align="center">{{ $notsponsored }}</h5>
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 100px;">
+                        <h5 class="card-title" align="center">{{ $notsponsored }}</h5>
+                    </div>
                     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                 </div>
             </div>
         </div>
-        <div class="col-md-6 col-lg-2">
-            <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                <div class="card-header" align="center">Jumlah Uang</div>
+        <div class="col-md-6 col-lg-3 mb-4 flex">
+            <div class="card text-dark bg-light mb-0 h-100">
+                <div class="card-header" align="center">
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 60px;">
+                        Jumlah Uang
+                    </div>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title" align="center">Rp. {{ number_format($totalamount, 2, ',', '.') }}</h5>
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 100px;">
+                        <h5 class="card-title" align="center">Rp. {{ number_format($totalamount, 2, ',', '.') }}</h5>
+                    </div>
                     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                 </div>
             </div>
         </div>
 
-        <div class="col-md-6 col-lg-2">
-            <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                <div class="card-header" align="center">Jumlah Sponsor Baru Bulan Ini</div>
+        <div class="col-md-6 col-lg-3 mb-4 flex">
+            <div class="card text-dark bg-light mb-0 h-100">
+                <div class="card-header" align="center">
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 60px;">
+                        Jumlah Sponsor Baru Bulan Ini
+                    </div>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title" align="center">{{ $newsponsor }}</h5>
+                    <h5 class="card-title" align="center">
+                        <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden"
+                            style="height: 100px;">
+                            {{ $newsponsor }}
+                        </div>
+                    </h5>
                     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                 </div>
             </div>
         </div>
 
-        <div class="col-sm-6 col-lg-2">
-            <div class="card text-dark bg-light mb-3" style="max-width: 18rem;">
-                <div class="card-header" align="center">Sponsor Yang Belum Bayar</div>
+        <div class="col-sm-6 col-lg-3 mb-4 flex">
+            <div class="card text-dark bg-light mb-0 h-100">
+                <div class="card-header" align="center">
+                    <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden" style="height: 60px;">
+                        Sponsor Yang Belum Bayar
+                    </div>
+                </div>
                 <div class="card-body">
-                    <h5 class="card-title" align="center">{{ $notpaid }}</h5>
+                    <h5 class="card-title" align="center">
+                        <div class="d-flex align-items-center justify-content-center text-break text-center overflow-hidden"
+                            style="height: 100px;">
+                            {{ $notpaid }}
+                        </div>
+                    </h5>
                     <!-- <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p> -->
                 </div>
             </div>
