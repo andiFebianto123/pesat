b819d2037994333ff50ae16759d07a65e145a5f5
danang <danangekoyudanto1995@gmail.com>
2022-02-10 14:26:41 +0700

    donate goods adjust remove button

--- app/Http/Controllers/Admin/DonateGoodsCrudController.php
+++ app/Http/Controllers/Admin/DonateGoodsCrudController.php
@@ -30,6 +30,11 @@ class DonateGoodsCrudController extends CrudController
         CRUD::setModel(\App\Models\DonateGoods::class);
         CRUD::setRoute(config('backpack.base.route_prefix') . '/donate-goods');
         CRUD::setEntityNameStrings('Donasi Barang', 'Donasi Barang');
+
+        $countGoods = DonateGoods::count();
+        if ($countGoods >= 1) {
+            $this->crud->denyAccess('create');
+        }
     }
 
     /**
@@ -49,10 +54,10 @@ class DonateGoodsCrudController extends CrudController
                 'type' => 'text',
             ],
         ]);
-        $countGoods = DonateGoods::all()->count();
-        if ($countGoods >= 1) {
-            $this->crud->denyAccess('create');
-        }
+
+        CRUD::button('delete')->remove();
+
+        $this->crud->addButtonFromView('line', 'delete_donation_goods', 'delete_donation_goods', 'ending');
     }
 
     /**
